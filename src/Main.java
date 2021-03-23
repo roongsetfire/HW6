6a2a54319bb58169321d0b3a9172a0818e8eb8eb
roong <roongsetfire@gmail.com>
2021-03-23 23:19:28 +0700

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+	public static void main(String[] args) {
+		char[] makeword = {'S','a','y',' ','H','i'};
+		String myword = new String(makeword);
+		System.out.println(myword);
+	}
+
+}
